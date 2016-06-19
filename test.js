fef9b8f85e47c01312d1a9bda309d73f39feb2b5
LyleLanley <pedfsk@gmail.com>
2016-06-19 15:34:28 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,38 @@
+var canvas = document.getElementById("canvas");
+var c = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var width = canvas.width;
+var height = canvas.height;
+var x = Math.floor((Math.random() * 255) + 1);
+
+
+
+
+
+
+/* function Update(){
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	c.clearRect(0,0,canvas.width, canvas.height);
+	c.fillRect(0, 0, canvas.width, canvas.height);
+	
+	window.requestAnimationFrame(Update);
+	
+}
+
+Update(); */
+
+window.addEventListener("orientationchange", function(){
+	var r = Math.floor((Math.random() * 255) + 1);
+	var g = Math.floor((Math.random() * 255) + 1);
+	var b = Math.floor((Math.random() * 255) + 1);
+    canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	c.fillStyle = 'rgb('+r+','+g+','+b+')';
+	c.clearRect(0,0,canvas.width, canvas.height);
+	c.fillRect(0, 0, canvas.width, canvas.height);
+	
+});
