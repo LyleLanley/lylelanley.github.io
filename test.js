3344f5cc0a60ab0cddb9d772bcaff637802ddb05
LyleLanley <pedfsk@gmail.com>
2016-06-19 16:00:48 +0200

    d

--- test.js
+++ test.js
@@ -25,13 +25,13 @@ var x = Math.floor((Math.random() * 255) + 1);
 
 Update(); */
 
-window.addEventListener("resize", function(){
+window.addEventListener("orientationchange", function(){
 	var r = Math.floor((Math.random() * 255) + 1);
 	var g = Math.floor((Math.random() * 255) + 1);
 	var b = Math.floor((Math.random() * 255) + 1);
     canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
-	/* c.fillStyle = 'rgb('+r+','+g+','+b+')'; */
+	c.fillStyle = 'rgb('+r+','+g+','+b+')';
 	c.clearRect(0,0,canvas.width, canvas.height);
 	c.fillRect(0, 0, canvas.width, canvas.height);
 	
